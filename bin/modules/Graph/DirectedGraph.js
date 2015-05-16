d033af25c08e0aafa84f4d7713721d5693494ba5
michalkurzeja <mike.kurzeja@gmail.com>
2015-05-16 12:25:06 +0200

    Graph change

--- bin/modules/Graph/DirectedGraph.js
+++ bin/modules/Graph/DirectedGraph.js
@@ -4,22 +4,22 @@ var Class = require('../utils/Class');
 var DirectedGraph = function(VerticesStorage, EdgeStorage) {
     Graph.call(this, VerticesStorage, EdgeStorage);
 
-    this.RemoveVertex = function(V) {
-        this.RemoveIncomingEdges(V);
-        this.RemoveIncidentEdges(V);
-        return this.verticesStorage.RemoveVertex(V.id);
+    this.RemoveVertex = function(id) {
+        this.RemoveIncomingEdges(id);
+        this.RemoveIncidentEdges(id);
+        return this.verticesStorage.RemoveVertex(id);
     };
 
-    this.GetIncomingEdges = function(V) {
-        return this.edgesStorage.GetIncomingEdges(V.id);
+    this.GetIncomingEdges = function(id) {
+        return this.edgesStorage.GetIncomingEdges(id);
     };
 
-    this.RemoveIncomingEdges = function(V) {
-        return this.edgesStorage.RemoveIncomingEdges(V.id);
+    this.RemoveIncomingEdges = function(id) {
+        return this.edgesStorage.RemoveIncomingEdges(id);
     };
 
-    this.GetEdgeBetween = function(Vsource, Vtarget) {
-        return this.edgesStorage.GetEdge(this.edgesStorage.GetEdgeBetween(Vsource.id, Vtarget.id));
+    this.GetEdgeBetween = function(source, target) {
+        return this.edgesStorage.GetEdge(this.edgesStorage.GetEdgeBetween(source, target));
     };
 
     return this;

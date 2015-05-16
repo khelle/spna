1bb3f9042346e1c5c57fa9f90a8a115047117330
michalkurzeja <mike.kurzeja@gmail.com>
2015-05-16 18:45:45 +0200

    Implemented Graph::MergeVertices

--- test/testGraph.js
+++ test/testGraph.js
@@ -2,16 +2,24 @@ var PTNGraph = require('../bin/modules/PTNGraph');  // load graph module
 
 var g = new PTNGraph('G1');     // create a new Graph
 
-var p1 = g.createPlace('p1',17);        // p1 [Markers: 17]
-var p2 = g.createPlace('p2');           // p2 [Markers: 1]
-var t1 = g.createTransition('t1',2);    // t1 [Priority: 2]
+var p1 = g.createPlace('p1',1);         // p1 [Markers: 1]
+var p2 = g.createPlace('p2', 2);        // p2 [Markers: 2]
+var p3 = g.createPlace('p3', 3);        // p3 [Markers: 3]
+//var p4 = g.createPlace('p4', 4);        // p4 [Markers: 4]
+var t1 = g.createTransition('t1',1);    // t1 [Priority: 1]
+var t2 = g.createTransition('t2',2);    // t2 [Priority: 2]
 
-p1.connect(t1,3);       // p1 -1-> t1
-t1.connect(p2,4);          // p1 -1-> t1 -4-> p2
+p1.connect(t1,1);          // p1 -1-> t1
+t1.connect(p2,2);          // p1 -1-> t1 -2-> p2
+p2.connect(t2,3);          // p1 -1-> t1 -2-> p2 -3-> t2
+t2.connect(p3,4);          // p1 -1-> t1 -2-> p2 -3-> t2 -4-> p3
 
-var g2 = g.clone();
+g.graph.MergeVertices(t1.id, t2.id);
+var i=1;
 
-console.log(g2);
+//var g2 = g.clone();
+//
+//console.log(g2);
 
 //t1.disconnect(p2).connect(p1);    // p1 <-1-> t1
 //t1.disconnect(p1).connect(p2,3);  // p1 -1-> t1 -3-> p2
